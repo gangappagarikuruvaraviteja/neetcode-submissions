c04957b786d4ec0ab638a3902175a3493101c1d4
GANGAPPA GARI KURUVA RAVI TEJA <gangappagari@karunya.edu.in>
2026-06-30 20:53:23 +0530

    Add: number-of-senior-citizens - submission-2

---
+++ Data Structures & Algorithms/number-of-senior-citizens/submission-2.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int countSeniors(String[] details) {
+
+
+       int count=0;
+
+       for(String s:details){
+        int age=Integer.parseInt(s.substring(11,13));
+        if(age>60){
+            count++;
+        }
+       }   
+       return count;
+       
+    }
+}
