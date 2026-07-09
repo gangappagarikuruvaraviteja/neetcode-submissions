3b66a64c480a33c30bb1e6d4e9daba46f22aee6c
GANGAPPA GARI KURUVA RAVI TEJA <gangappagari@karunya.edu.in>
2026-07-09 22:55:05 +0530

    Add: max-water-container - submission-0

---
+++ Data Structures & Algorithms/max-water-container/submission-0.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int maxArea(int[] heights) {
+        
+
+            int max=0;
+        for(int i=0;i<heights.length;i++){
+            for(int j=i+1;j<heights.length;j++){
+
+                int area=(j-i)*Math.min(heights[i],heights[j]);
+                max=Math.max(area,max);
+            }
+        }
+        return max;
+
+    }
+}
