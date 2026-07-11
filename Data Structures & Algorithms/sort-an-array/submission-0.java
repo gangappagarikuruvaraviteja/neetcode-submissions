6edc9b182e780fcf08b326c23ad71fa8c7de32b6
GANGAPPA GARI KURUVA RAVI TEJA <gangappagari@karunya.edu.in>
2026-07-11 14:31:37 +0530

    Add: sort-an-array - submission-0

---
+++ Data Structures & Algorithms/sort-an-array/submission-0.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[] sortArray(int[] nums) {
+        // int n=nums.length;
+        // int []arr=new int[n];
+        // int large=nums[0];
+        // for(int i=nums.length-1;i>=0;i--){
+        //     if(nums[i]>large){
+                
+        //         arr[i]=nums[i];
+        //         large++;
+
+        //     }
+        // }
+        // return arr;
+
+        Arrays.sort(nums);
+
+        return nums;
+    }
+}
