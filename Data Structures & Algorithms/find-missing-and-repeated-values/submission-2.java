bfb9ef3daa0744fee199556cc470d8bc10635cb5
GANGAPPA GARI KURUVA RAVI TEJA <gangappagari@karunya.edu.in>
2026-07-11 11:43:57 +0530

    Add: find-missing-and-repeated-values - submission-2

---
+++ Data Structures & Algorithms/find-missing-and-repeated-values/submission-2.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int[] findMissingAndRepeatedValues(int[][] grid) {
+        
+             HashMap<Integer,Integer>map=new HashMap<>();
+          int n=grid.length;
+        for(int []row:grid){
+          for(int num:row){
+               map.put(num,map.getOrDefault(num,0)+1);
+          }}
+          int rem=0;
+          int rep=0;
+          for(int i=1;i<=n*n;i++){
+               int count=map.getOrDefault(i,0);
+
+               if(count==2){
+                    rep=i;
+               }
+               
+               if(count==0){
+                    rem=i;
+               }
+
+          
+        }
+
+        return new int[]{rep,rem};
+        
+    }
+}
