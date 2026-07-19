618d7dbf1cfa1260d03529b4a7340857dd82f855
GANGAPPA GARI KURUVA RAVI TEJA <gangappagari@karunya.edu.in>
2026-07-19 10:54:36 +0530

    Add: find-target-in-rotated-sorted-array - submission-2

---
+++ Data Structures & Algorithms/find-target-in-rotated-sorted-array/submission-2.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int search(int[] nums, int target) {
+
+        int left = 0;
+        int right = nums.length - 1;
+
+    //    Arrays.sort(nums);
+
+       for(int i=0;i<nums.length;i++){
+        if(nums[i]==target){
+            return i;
+        }
+       }
+       return -1;
+    }
+}
