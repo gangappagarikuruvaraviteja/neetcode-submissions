0a92246fbaae946c1f67abbe5415e588f0d83cca
GANGAPPA GARI KURUVA RAVI TEJA <gangappagari@karunya.edu.in>
2026-07-20 23:25:08 +0530

    Add: longest-repeating-substring-with-replacement - submission-5

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-5.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int characterReplacement(String s, int k) {
+        int max=0;
+        for(int target='A';target<='Z';target++){
+          int left=0;
+          int changes=0;
+          for(int right=0;right<s.length();right++){
+              if(s.charAt(right)!=target){
+                changes++;
+              }
+              while(changes>k){
+                if(s.charAt(left)!=target){
+                changes--;
+                }
+                left++;
+              }
+              max=Math.max(max,right-left+1);
+          }
+        }    
+        return max;
+    }
+}
