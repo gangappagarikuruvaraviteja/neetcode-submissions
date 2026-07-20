a3759545c46260e835b865944a756d09467a696b
GANGAPPA GARI KURUVA RAVI TEJA <gangappagari@karunya.edu.in>
2026-07-20 23:09:15 +0530

    Add: longest-substring-without-duplicates - submission-2

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-2.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        
+          HashSet<Character> set=new HashSet<>();
+          int left=0;
+          int max=0;
+
+          for(int i=0;i<s.length();i++){
+
+              char c=s.charAt(i);
+
+              while(set.contains(c)){
+              set.remove(s.charAt(left));
+              left++;
+              }
+
+
+
+              set.add(c);
+
+              max=Math.max(max,i-left+1);
+          }
+          return max;
+    }
+}
