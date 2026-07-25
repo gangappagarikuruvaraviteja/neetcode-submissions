495e538687f75a295254ee605d16b7c75bf22621
GANGAPPA GARI KURUVA RAVI TEJA <gangappagari@karunya.edu.in>
2026-07-25 22:05:32 +0530

    Add: is-anagram - submission-7

---
+++ Data Structures & Algorithms/is-anagram/submission-7.java
@@ -0,0 +1,60 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+  
+
+            HashMap<Character, Integer> map=new HashMap<>();
+
+        if(s.length()!=t.length()){
+            return false;
+        }
+
+        for(char ch:s.toCharArray()){
+
+            map.put(ch,map.getOrDefault(ch,0)+1);
+
+
+        }
+
+        for(char ch:t.toCharArray()){
+            if(!map.containsKey(ch)){
+                return false;
+            }
+
+            map.put(ch,map.get(ch)-1);
+            if(map.get(ch)==0){
+                map.remove(ch);
+
+                
+            }
+
+            }
+        return true;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        // if(s.length()!=t.length()){
+        //     return false;
+        // }
+
+        // char[] a=s.toCharArray();
+        // char[] b=t.toCharArray();
+
+        // Arrays.sort(a);
+        // Arrays.sort(b);
+
+        // return Arrays.equals(a,b);
+      
+    }
+}
