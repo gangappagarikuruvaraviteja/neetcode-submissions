f932513be4d16b91a622d5762c7c618139a167e5
GANGAPPA GARI KURUVA RAVI TEJA <gangappagari@karunya.edu.in>
2026-07-26 11:42:50 +0530

    Add: first-missing-positive - submission-0

---
+++ Data Structures & Algorithms/first-missing-positive/submission-0.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int firstMissingPositive(int[] nums) {
+
+
+            HashSet<Integer> set=new HashSet<>();
+
+            for(int num:nums)
+                {
+                    if(num>0){
+                        set.add(num);
+                    }
+                }
+
+            int i=1;
+
+            while(true){
+
+                if(!set.contains(i)){
+                    return i;
+                }
+                i++;
+            }    
+           
+
+
+
+
+    }
+}
