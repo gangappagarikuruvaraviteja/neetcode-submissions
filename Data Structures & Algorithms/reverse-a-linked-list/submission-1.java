5d965d78da1a469eaa814cf098bc4399dc72f39d
GANGAPPA GARI KURUVA RAVI TEJA <gangappagari@karunya.edu.in>
2026-07-31 23:52:23 +0530

    Add: reverse-a-linked-list - submission-1

---
+++ Data Structures & Algorithms/reverse-a-linked-list/submission-1.java
@@ -0,0 +1,45 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        // ListNode prev=null;
+
+        // ListNode curr=head;
+
+        // while(curr!=null){
+        //     ListNode next=curr.next;
+        //     curr.next=prev;
+        //     prev=curr;
+        //     curr=next;
+        // }
+        // head =prev;
+
+        if(head==null)return null;
+        Stack<ListNode> stack=new Stack<>();
+        ListNode temp=head;
+
+        while(temp!=null){
+            stack.push(temp);
+            temp=temp.next;
+        }
+        head=stack.pop();
+        temp=head;
+
+        while(!stack.isEmpty()){
+            temp.next=stack.pop();
+            temp=temp.next;
+        }
+        temp.next=null;
+
+        return head;
+    }
+}
