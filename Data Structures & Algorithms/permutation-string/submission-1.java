aee315e4405c0ffa5622e49928a5cf41180ab1a0
GANGAPPA GARI KURUVA RAVI TEJA <gangappagari@karunya.edu.in>
2026-08-09 16:16:59 +0530

    Add: permutation-string - submission-1

---
+++ Data Structures & Algorithms/permutation-string/submission-1.java
@@ -0,0 +1,51 @@
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+       
+
+
+       if(s1.length()>s2.length()){
+        return false;
+       }
+
+        HashMap<Character,Integer> map1=new HashMap<>();
+        HashMap<Character,Integer> map2=new HashMap<>();
+
+
+        for(char ch:s1.toCharArray()){
+            map1.put(ch,map1.getOrDefault(ch,0)+1);
+        }
+
+        int left=0;
+
+        for(int i=0;i<s2.length();i++){
+
+            char ch=s2.charAt(i);
+
+            map2.put(ch,map2.getOrDefault(ch,0)+1);
+           
+
+            if(i-left+1>s1.length()){
+                char remove=s2.charAt(left);
+
+                map2.put(remove,map2.get(remove)-1);
+            
+
+            if(map2.get(remove)==0){
+                map2.remove(remove);
+
+            }
+            left++;
+
+            }
+
+            if(map1.equals(map2)){
+                return true;
+            }
+
+        }
+
+        return false;
+
+
+    }
+}
