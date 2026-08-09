5c94c5b4d5e7b5a04a4742b3cd20e21c9bc07413
GANGAPPA GARI KURUVA RAVI TEJA <gangappagari@karunya.edu.in>
2026-08-09 16:38:09 +0530

    Add: validate-parentheses - submission-3

---
+++ Data Structures & Algorithms/validate-parentheses/submission-3.java
@@ -0,0 +1,39 @@
+class Solution {
+    public boolean isValid(String s) {
+
+    Stack<Character> stack=new Stack<>();
+
+        for(char ch:s.toCharArray()){
+
+            // char ch=s.charAt(i);
+
+            if(ch=='(' || ch=='{' || ch=='['){
+
+                    stack.push(ch);
+
+            }
+            else{
+
+                if(stack.isEmpty()){
+                    return false;
+                }
+
+                if(ch==')'  && stack.peek() !='('){
+                    return false;
+                }
+                if(ch=='}' && stack.peek() !='{'){
+                    return false;
+                }
+                if(ch==']' && stack.peek() !='['){
+                    return false;
+                }
+
+                stack.pop();
+            
+            }
+        }
+        return stack.isEmpty();
+
+        
+    }
+}
