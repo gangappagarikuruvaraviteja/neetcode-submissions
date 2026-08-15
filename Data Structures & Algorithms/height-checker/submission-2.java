32b41f704ed7f43f9854e75357e3f3c52accb826
GANGAPPA GARI KURUVA RAVI TEJA <gangappagari@karunya.edu.in>
2026-08-15 23:34:51 +0530

    Add: height-checker - submission-2

---
+++ Data Structures & Algorithms/height-checker/submission-2.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int heightChecker(int[] heights) {
+
+
+        int count=0;
+
+        int[]expected=heights.clone();
+        Arrays.sort(heights);
+
+        for(int i=0;i<heights.length;i++){
+            if(heights[i]!=expected[i]){
+                count++;
+            }
+        }
+        
+        return count;
+    }
+}
