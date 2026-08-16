d29d40d3ed51be56cf90d202e9db5a85e1b73e8a
GANGAPPA GARI KURUVA RAVI TEJA <gangappagari@karunya.edu.in>
2026-08-16 23:39:54 +0530

    Add: unique-email-addresses - submission-1

---
+++ Data Structures & Algorithms/unique-email-addresses/submission-1.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int numUniqueEmails(String[] emails) {
+
+        HashSet<String>set=new HashSet<>();
+
+        for(String email:emails){
+
+            int at=email.indexOf("@");
+            String local=email.substring(0,at);
+            String domain=email.substring(at);
+
+            local=local.split("\\+")[0];
+            local=local.replace(".","");
+
+            set.add(local+domain);
+        }
+        return set.size();
+    }
+}
