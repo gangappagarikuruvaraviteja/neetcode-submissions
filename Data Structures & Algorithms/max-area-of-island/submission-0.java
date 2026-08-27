2128ea2fb44dddcd5f4b002c5cbfcf214d5783bb
GANGAPPA GARI KURUVA RAVI TEJA <gangappagari@karunya.edu.in>
2026-08-27 23:57:38 +0530

    Add: max-area-of-island - submission-0

---
+++ Data Structures & Algorithms/max-area-of-island/submission-0.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int maxAreaOfIsland(int[][] grid) {
+
+        int maxarea=0;
+        for(int i=0;i<grid.length;i++){
+            for(int j=0;j<grid[0].length;j++){
+               if(grid[i][j]==1){
+                int area=dfs(grid,i,j);
+                maxarea=Math.max(area,maxarea);
+               }
+            }
+        }
+        return maxarea;
+    }
+    public int dfs(int [][]grid,int r,int c){
+        if(r<0 || r>=grid.length || c<0 || c>=grid[0].length || grid[r][c]==0){
+            return 0;
+        }
+        grid[r][c]=0;
+        int area=1;
+        area+=dfs(grid,r-1,c);
+        area+=dfs(grid,r+1,c);
+        area+=dfs(grid,r,c-1);
+        area+=dfs(grid,r,c+1);
+        return area;
+    }
+}
