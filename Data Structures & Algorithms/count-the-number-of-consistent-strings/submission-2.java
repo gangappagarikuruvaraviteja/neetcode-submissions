bbdef8d979678f3532e6a0648fda0b15edd612f3
GANGAPPA GARI KURUVA RAVI TEJA <gangappagari@karunya.edu.in>
2026-08-29 22:47:32 +0530

    Add: count-the-number-of-consistent-strings - submission-2

---
+++ Data Structures & Algorithms/count-the-number-of-consistent-strings/submission-2.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int countConsistentStrings(String allowed, String[] words) {
+
+        HashSet<Character>set=new HashSet<>();
+
+        for(char ch:allowed.toCharArray()){
+            set.add(ch);
+        }
+    int count=0;
+        for(String word:words){
+            int valid=0;
+
+            for(char ch:word.toCharArray()){
+                if(set.contains(ch)){
+                    valid++;
+                }
+
+                if(valid==word.length()){
+                    count++;
+                }
+            }
+        }
+        return count;
+        
+    }
+}
