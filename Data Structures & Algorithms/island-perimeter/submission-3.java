1b061e1721be010ced2bb35dccd67b1af140d614
GANGAPPA GARI  RAVI TEJA <gangappagari@karunya.edu.in>
2026-09-03 22:22:16 +0530

    Add: island-perimeter - submission-3

---
+++ Data Structures & Algorithms/island-perimeter/submission-3.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int islandPerimeter(int[][] grid) {
+
+        int pre=0;
+
+        for(int i=0;i<grid.length;i++){
+            for(int j=0;j<grid[0].length;j++){
+                if(grid[i][j]==1){
+                    pre+=4;
+
+                    if(i>0 && grid[i-1][j]==1){
+                        pre-=2;
+                    }
+                    if(j>0 && grid[i][j-1]==1){
+                        pre-=2;
+                    }
+                }
+            }
+        }
+
+        return pre;
+        
+    }
+}
